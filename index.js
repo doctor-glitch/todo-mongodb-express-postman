60a620514f1d4a0d64f78bd1103a658827f95396
doctor-glitch <harinrajan2014@gmail.com>
2020-01-06 12:47:02 +0530

    database connection established

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-
-const todo = require('./todo/todo');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const todo = require("./todo/todo");
 const app = express();
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get('/', function(req,res){
-    const todos = todo.getTodo();
+app.use(bodyParser.json());
+
+
+app.get("/", function (req, res) {
+  todo.getTodo().then(todos => {
     res.json(todos);
-})
+  })
+});
 
-app.post('/create', function(req,res){
-    const todos = todo.addTodo(req.body);
-    res.json(todos);
-})
+app.post("/create", function (req, res) {
+  todo.addTodo(req.body).then(data => {
+    res.json(data);
+  })
+});
 
-app.put('/edit/:id', function(req,res){
-    const todos = todo.editTodo(req.params.id,req.body.name,req.body.description);
-    res.json(todos);
-})
+app.put("/edit/:id", function (req, res) {
+  const todos = todo.editTodo(
+    req.params.id,
+    req.body.name,
+    req.body.description
+  );
+  res.json(todos);
+});
 
-app.delete('/delete/:id', function(req,res){
-    const id = req.params.id;
-    const todos = todo.deleteTodo(id);
-    res.json(todos);
-})
+app.delete("/delete/:id", function (req, res) {
+  const id = req.params.id;
+  const todos = todo.deleteTodo(id);
+  res.json(todos);
+});
 
-app.listen(port, function(){
-    console.log(`Example app listening on port ${port}!`)
-})
-
-
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
+});
--- todo/todo.js
+++ todo/todo.js
@@ -1,32 +1,34 @@
+const db = require("../db");
+
 let todos = [
-    { id:0, name:"todo1", description:"todo1 desc"},
-    { id:1, name:"todo2", description:"todo2 desc"},
-    {id:2, name:"todo3", description:"todo3 desc"},
-]
-function addTodo(todo){
-    todos.push(todo);
-    return todos;
+  { id: 0, name: "todo1", description: "todo1 desc" },
+  { id: 1, name: "todo2", description: "todo2 desc" },
+  { id: 2, name: "todo3", description: "todo3 desc" }
+];
+function addTodo(todo) {
+  const newTodo=new db.Todo(todo);
+  return newTodo.save();
 }
-function editTodo(id, name, description){
-    let todo=todos.find(function(todo){
-        return todo.id==id;
-    });
-    todo.name = name;
-    todo.description = description;
-    return todos;
+function editTodo(id, name, description) {
+  let todo = todos.find(function (todo) {
+    return todo.id == id;
+  });
+  todo.name = name;
+  todo.description = description;
+  return todos;
 }
-function getTodo(){
-    return todos;
+function getTodo() {
+  return db.Todo.find({});
 }
-function deleteTodo(id){
-    todos=todos.filter(function(todo){
-        return todo.id!=id;
-    });
-    return todos;
+function deleteTodo(id) {
+  todos = todos.filter(function (todo) {
+    return todo.id != id;
+  });
+  return todos;
 }
-var name="todo";
-exports.addTodo =addTodo;
-exports.getTodo =getTodo;
-exports.deleteTodo =deleteTodo;
-exports.name =name;
+var name = "todo";
+exports.addTodo = addTodo;
+exports.getTodo = getTodo;
+exports.deleteTodo = deleteTodo;
+exports.name = name;
 exports.editTodo = editTodo;
