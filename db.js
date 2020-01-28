109d4d952f7a61ee5c0874daafa6fa0f3a8367c0
doctor-glitch <harinrajan2014@gmail.com>
2020-01-28 14:17:31 +0530

    msg

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/todo", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/todo", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const Todo = mongoose.model("Todo", { name: String, description: String, userId: mongoose.Schema.Types.ObjectId });
 
